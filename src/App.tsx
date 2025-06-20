8e84e1cb487d22875db9f25414539cdb946993b9
adept-fray <siavash.kh41@gmail.com>
2025-06-20 16:50:34 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <>
+      <h1>Welcome to CCPedigree</h1>
+      <section className="hero">
+        <span>Dog icon</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste illum,
+          doloremque, est ipsam totam, pariatur at vitae dolorem cumque facilis
+          sapiente sit necessitatibus? Pariatur, est? Reprehenderit corporis ex
+          quibusdam nobis?
+        </p>
+        <p>
+          this site will be about rebuilding an old website for people to share
+          and upload their dogs info. like a social platform such as facebook
+          but for dogs.
+        </p>
+      </section>
+      <footer>
+        <span>© CCPedigree</span>
+      </footer>
+    </>
+  );
+}
+
+export default App;
